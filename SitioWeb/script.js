d5753ce9a29ea23ffd837c7ef084226283742bed
Diego-CGTZ <126724261+Diego-CGTZ@users.noreply.github.com>
2023-05-17 00:17:30 -0600

    Cambios en el sitio web

--- SitioWeb/script.js
+++ SitioWeb/script.js
@@ -3,6 +3,5 @@ const iconMenu = document.querySelector(".icon-menu")
 const ulMenu = document.querySelector(".menu-container");
 
 iconMenu.addEventListener("click", function(){
-    console.log("pene");
     console.log(ulMenu.classList.toggle("hidden"));
 })
